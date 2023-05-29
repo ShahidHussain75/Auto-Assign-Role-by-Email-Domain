c6c9dc15a8884f9d21e7a5dad1ae360a362a63b3
koolrock75 <42839149+koolrock75@users.noreply.github.com>
2023-05-29 15:52:19 +0500

    Update domain-registration.php

--- domain-registration.php
+++ domain-registration.php
@@ -1,36 +1,196 @@
 <?php
-/*
-Plugin Name: Chatbot Support Agent
-Plugin URI: http://yourwebsite.com
-Description: A WordPress plugin for integrating a chatbot support agent using the ChatGPT API.
-Version: 1.0.0
-Author: Your Name
-Author URI: http://yourwebsite.com
-License: GPL2
-*/
+/**
+ * Plugin Name: Auto Assign Role by Email Domain
+ * Plugin URI:  https://hocien.com/auto-assign-role/
+ * Description: The "Auto Assign Role by Email Domain" plugin allows you to assign user roles based on their email addresses during registration. If a user's role has expired according to the specified time set in the plugin's settings, their role will be automatically changed to "Subscriber". Additionally, if the administrator deletes a domain rule in the plugin's settings page, all users with that specific domain name will have their roles changed to "Subscriber".
+ * Version: 2.0.0
+ * Author: Shahid Hussain
+ * Author URI: https://www.upwork.com/freelancers/~01304587883757d540
+ * Version: 1.0.0
+ * License: GPL2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: Auto-Assign-Role
+ */
 
-// Enqueue necessary scripts and styles
-function chatbot_support_agent_enqueue_scripts() {
-    wp_enqueue_script('chatbot-support-agent-script', plugins_url('js/chatbot-support-agent.js', __FILE__), array('jquery'), '1.0', true);
+// Add admin menu
+add_action('admin_menu', 'domain_registration_menu');
 
-    // Pass API key, instructions, and avatar image URL to the JavaScript file
-    $api_key = get_option('chatbot-support-agent-api-key');
-    $instructions = get_option('chatbot-support-agent-instructions');
-    $avatar_url = plugins_url('images/avatar.png', __FILE__); // Replace 'images/avatar.png' with the actual path to your avatar image
-    wp_localize_script('chatbot-support-agent-script', 'chatbotSupportAgentData', array(
-        'api_key' => $api_key,
-        'instructions' => $instructions,
-        'avatar_url' => $avatar_url
-    ));
-
-    wp_enqueue_style('chatbot-support-agent-style', plugins_url('css/chatbot-support-agent.css', __FILE__));
+function domain_registration_menu() {
+    add_options_page('Domain Registration Settings', 'Domain Registration', 'manage_options', 'domain-registration', 'domain_registration_settings_page');
 }
-add_action('wp_enqueue_scripts', 'chatbot_support_agent_enqueue_scripts');
 
-// Add the chatbot interface to the WordPress website
-function chatbot_support_agent_add_interface() {
-    echo '<div id="chatbot-support-agent"></div>';
+// Enqueue jQuery UI datepicker script and style
+add_action('admin_enqueue_scripts', 'domain_registration_enqueue_scripts');
+
+function domain_registration_enqueue_scripts() {
+    wp_enqueue_script('jquery-ui-datepicker');
+    wp_enqueue_style('jquery-ui-datepicker-style', '//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css');
 }
-add_action('wp_footer', 'chatbot_support_agent_add_interface');
 
-// Rest of the code...
+// Settings page
+function domain_registration_settings_page() {
+    if (!current_user_can('manage_options')) {
+        wp_die(__('You do not have sufficient permissions to access this page.'));
+    }
+
+    domain_registration_delete();
+    domain_registration_add();
+
+    $domains = get_option('domain_registration_domains', array());
+    ?>
+    <div class="wrap">
+        <h1>Domain Registration Settings</h1>
+        <form method="post" action="">
+            <table class="form-table">
+                <tr valign="top">
+                    <th scope="row">Add Domain</th>
+                    <td>
+                        <input type="text" name="add_domain" placeholder="example.com" required />
+                        <select name="add_role">
+                            <?php wp_dropdown_roles(); ?>
+                        </select>
+                        <input type="text" name="add_expiration" id="expiration_date" placeholder="Expiration date" required />
+                        <input type="submit" class="button button-primary" value="Add" />
+                    </td>
+                </tr>
+            </table>
+        </form>
+        <hr />
+        <h2>Current Domains</h2>
+        <table class="wp-list-table widefat fixed striped">
+            <thead>
+                <tr>
+                    <th>Domain</th>
+                    <th>Role</th>
+                    <th>Expiration</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($domains as $domain => $data) : ?>
+                    <?php
+                    $expiration_date = strtotime($data['expiration']);
+                    $remaining_time = $expiration_date - time();
+                    $days_remaining = floor($remaining_time / (24 * 60 * 60));
+                    $hours_remaining = floor(($remaining_time % (24 * 60 * 60)) / (60 * 60));
+                    $minutes_remaining = floor(($remaining_time % (60 * 60)) / 60);
+                    $seconds_remaining = $remaining_time % 60;
+                    ?>
+                    <tr>
+                        <td><?php echo $domain; ?></td>
+                        <td><?php echo $data['role']; ?></td>
+                        <td><?php echo "$days_remaining days $hours_remaining hours $minutes_remaining minutes $seconds_remaining seconds"; ?></td> <!-- Display countdown -->
+                        <td>
+                            <form method="post" action="">
+                                <input type="hidden" name="delete_domain" value="<?php echo $domain; ?>" />
+                                <input type="submit" class="button button-secondary" value="Delete" />
+                            </form>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+    <script>
+        jQuery(document).ready(function($) {
+            $('#expiration_date').datepicker({
+                minDate: 0, // Prevent selecting past dates
+                dateFormat: 'yy-mm-dd' // Set the desired date format
+            });
+        });
+    </script>
+    <?php
+}
+
+// Add domain
+function domain_registration_add() {
+    if (isset($_POST['add_domain']) && isset($_POST['add_role']) && isset($_POST['add_expiration'])) {
+        $domain = sanitize_text_field($_POST['add_domain']);
+        $role = sanitize_text_field($_POST['add_role']);
+        $expiration = sanitize_text_field($_POST['add_expiration']);
+
+        if (empty($expiration)) {
+            echo '<div class="error"><p><strong>' . __('Expiration date is required.', 'menu-test') . '</strong></p></div>';
+            return;
+        }
+
+        $expiration_timestamp = strtotime($expiration);
+
+        if ($expiration_timestamp === false || $expiration_timestamp < time()) {
+            echo '<div class="error"><p><strong>' . __('Invalid expiration date. Please enter a future date.', 'menu-test') . '</strong></p></div>';
+            return;
+        }
+
+        $domains = get_option('domain_registration_domains', array());
+
+        if (isset($domains[$domain])) {
+            echo '<div class="error"><p><strong>' . __('Domain already exists. Please use a different domain.', 'menu-test') . '</strong></p></div>';
+            return;
+        }
+
+        $domains[$domain] = array('role' => $role, 'expiration' => $expiration);
+        update_option('domain_registration_domains', $domains);
+
+        // Update existing users with the specified domain
+        update_existing_users_role_by_domain($domain, $role, $expiration);
+
+        echo '<div class="updated"><p><strong>' . __('Domain added.', 'menu-test') . '</strong></p></div>';
+    }
+}
+// Update existing users' role by domain
+function update_existing_users_role_by_domain($domain, $role, $expiration) {
+    $users = get_users();
+    foreach ($users as $user) {
+        $user_email_domain = substr(strrchr($user->user_email, "@"), 1);
+        if ($user_email_domain === $domain) {
+            $user->set_role($role);
+            $expiration_date = strtotime($expiration);
+
+            update_user_meta($user->ID, 'domain_registration_expiration', $expiration_date);
+        }
+    }
+}
+
+// Delete domain
+function domain_registration_delete() {
+    if (isset($_POST['delete_domain'])) {
+        $domain = sanitize_text_field($_POST['delete_domain']);
+        change_role_to_subscriber_by_domain($domain);
+        $domains = get_option('domain_registration_domains', array());
+        unset($domains[$domain]);
+        update_option('domain_registration_domains', $domains);
+
+        echo '<div class="updated"><p><strong>' . __('Domain deleted.', 'menu-test') . '</strong></p></div>';
+    }
+}
+
+// Change role to subscriber by domain
+function change_role_to_subscriber_by_domain($domain) {
+    $users = get_users();
+    foreach ($users as $user) {
+        $user_email_domain = substr(strrchr($user->user_email, "@"), 1);
+        if ($user_email_domain === $domain) {
+            $user->set_role('subscriber');
+        }
+    }
+}
+
+// Restrict registration to specific domain names start
+add_action('registration_errors', 'domain_registration_restrict_registration', 10, 3);
+
+function domain_registration_restrict_registration($errors, $sanitized_user_login, $user_email) {
+    $allowed_domains = array();
+    $domains = get_option('domain_registration_domains', array());
+
+    foreach ($domains as $domain => $data) {
+        $allowed_domains[] = $domain;
+    }
+
+    $user_email_domain = substr(strrchr($user_email, "@"), 1);
+    if (!in_array($user_email_domain, $allowed_domains)) {
+        $errors->add('domain_registration_error', __('Registration is restricted to specific domain names.'));
+    }
+
+    return $errors;
+}
+// Restrict registration to specific domain names end
